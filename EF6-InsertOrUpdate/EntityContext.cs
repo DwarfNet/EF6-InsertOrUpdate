04aaeb2ff7b7d7dbf3f5b32672346daf80fd20a4
Dwarf Net <DwarfNet@outlook.com>
2020-10-25 20:16:03 +0100

    Final Version

--- EF6-InsertOrUpdate/EntityContext.cs
+++ EF6-InsertOrUpdate/EntityContext.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Design;
 
 namespace EF6_InsertOrUpdate
 {
-    class EntityContext
+    public class EntityContext : DbContext
     {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public void InsertOrUpdate<T>(T entity) where T : class
+        {
+            DbSet<T> dbSet = this.Set<T>();
+            var entry = this.Entry(entity);
+
+            if (entry.State == EntityState.Detached || entry.State == EntityState.Added)
+                    dbSet.Add(entity);
+        }
     }
 }
---
+++ EF6-InsertOrUpdate/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6_InsertOrUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EntityContext())
+            {
+                List<Blog> blogs;
+                CleanCurrentDb(db);
+
+                var name = "BlogTest1";
+                Console.WriteLine($"Create Blog {name}.");
+                var originalBlog = new Blog { Name = name };
+
+                Console.WriteLine($"Add 'Original' Post into the blog.");
+                originalBlog.Posts = new List<Post>()
+                {
+                    new Post() {
+                        Title = "My Original Title",
+                        Content = "My original content."
+                        }
+                };
+
+                Console.WriteLine($"Add Blog {originalBlog.Name} into db.");
+                db.Blogs.Add(originalBlog);
+                
+                Console.WriteLine($"Save Blog {originalBlog.Name} into db.");
+                db.SaveChanges();
+
+                Console.WriteLine("All blogs in the database:");
+                blogs = db.Blogs.ToList();
+                foreach (var blog in blogs)
+                {
+                    Console.WriteLine($"Blog ID: {blog.BlogId}.\tBlog NAME: {blog.Name}.");
+                }
+                
+                Console.WriteLine($"Get Blog where Name is {name}.");
+                var oldBlog = db.Blogs.Where(b => b.Name == name).FirstOrDefault();
+                Console.WriteLine($"Blog ID is : {oldBlog.BlogId}.");
+                Console.WriteLine($"Change Name for {oldBlog.Name}.");
+                oldBlog.Name = "The new Blog";
+                
+                Console.WriteLine($"Insert or update the original blog version.");
+                db.InsertOrUpdate(originalBlog);
+                Console.WriteLine($"Insert or update the new blog version.");
+                db.InsertOrUpdate(oldBlog);
+
+                Console.WriteLine($"Save Blog {oldBlog.Name} into db.");
+                db.SaveChanges();
+
+                Console.WriteLine("All blogs in the database:");
+                blogs = db.Blogs.ToList();
+                foreach (var blog in blogs)
+                {
+                    Console.WriteLine($"Blog ID: {blog.BlogId}.\tBlog NAME: {blog.Name}.");
+                }
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+        private static void CleanCurrentDb(EntityContext db)
+        {
+            foreach (var blog in db.Blogs)
+            {
+                db.Blogs.Remove(blog);
+            }
+        }
+    }
+}
